97ee692e46d07792bcad9af037d2e452a53e009b
medihebfaiza <fmohamediheb@gmail.com>
2018-05-23 18:32:06 +0200

    change sh

--- executeBattleShip.sh
+++ executeBattleShip.sh
@@ -3,6 +3,6 @@
 javac -d bin src/gayet/simon/*.java;
 cd bin;
 java gayet.simon.Battleship ; 
-rm gayet/simon/*.class;
+
 cd ..; 
  
--- executeTestIA.sh
+++ executeTestIA.sh
@@ -5,9 +5,6 @@ javac -cp bin -d bin src/fr/battleship/*.java;
 cd bin;
 java fr.battleship.TestIA ; 
 
-rm gayet/simon/*.class;
-rm fr/battleship/*.class;
-
 cp ai_proof.csv ../ai_proof.csv;
 rm ai_proof.csv;
 cd ..; 
